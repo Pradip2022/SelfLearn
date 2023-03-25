919301fbb7149faadcd4cb8e36c72e07b65e18da
pradip budathoki <pradipbudathoki2018@gmail.com>
2023-03-25 03:06:37 -0500

    added the challenge

--- typescript-react/src/component/ButtonIncrement.tsx
+++ typescript-react/src/component/ButtonIncrement.tsx
@@ -1,8 +1,15 @@
 import { FC, useState } from "react"
 export const ButtonIncrement:FC = ()=>{
     const [counter, setCounter] = useState(0); 
+    const [challengeCounter, setChallengeCounter] = useState(5); 
     const clicked = () =>setCounter(counter+1)
+    const challengeClicked = () =>setChallengeCounter(challengeCounter*2)
     return(
-        <button onClick = {clicked}>{counter}</button>
+        <div>
+            <button onClick = {clicked}>{counter}</button>
+            <h1>challenge: make the label show 5 at launch and double the value every hit </h1>
+            <button onClick = {challengeClicked}>{challengeCounter}</button>
+        </div>
+
     )
 }
