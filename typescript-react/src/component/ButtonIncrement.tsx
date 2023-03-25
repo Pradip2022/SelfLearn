f5ff401b6309008499176eb4f800e60694f0d98c
pradip budathoki <pradipbudathoki2018@gmail.com>
2023-03-25 02:58:36 -0500

    made the clicked method into single line

--- typescript-react/src/component/ButtonIncrement.tsx
+++ typescript-react/src/component/ButtonIncrement.tsx
@@ -1,9 +1,7 @@
 import { FC, useState } from "react"
 export const ButtonIncrement:FC = ()=>{
     const [counter, setCounter] = useState(0); 
-    const clicked = () =>{
-        setCounter(counter+1)
-    }
+    const clicked = () =>setCounter(counter+1)
     return(
         <button onClick = {clicked}>{counter}</button>
     )
